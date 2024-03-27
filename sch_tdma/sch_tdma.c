854ea95f628923acf3c5408318e180abee42317d
Gennadi Ryan <gennadiryan@Gennadis-MacBook-Air.local>
2024-03-26 22:28:17 -0400

    Fixed TDMA burst pattern; all packets now buffered during downtime and transmitted during uptime

--- sch_tdma/sch_tdma.c
+++ sch_tdma/sch_tdma.c
@@ -18,7 +18,7 @@
 #include <net/pkt_cls.h>
 #include <net/pkt_sched.h>
 
-#include "tc_tdma.h" // TODO: change tc_tbf_test_qopt to tc_tdma
+#include "tc_tdma.h" // TODO: change tc_tbf_test_qopt to tc_tdma; must do the same in the tc cli implementation
 
 
 /*	Simple Token Bucket Filter.
