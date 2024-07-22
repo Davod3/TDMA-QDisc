3cade1b1a36ce82b9796f6cfb8b86877a228731a
Davod3 <davidpereira.pereira4@gmail.com>
2024-07-22 11:44:21 +0100

    Created manage_qdisc to add new qdisc to tc

---
+++ netcntlr/manage_qdisc.h
@@ -0,0 +1,21 @@
+#include <linux/types.h>
+
+struct tc_tdma_qopt {
+	__u32		limit;
+
+	__s64		t_frame;
+	__s64		t_slot;
+	__s64		t_offset;
+};
+
+
+enum {
+	TCA_TDMA_UNSPEC,
+	TCA_TDMA_PARMS,
+	TCA_TDMA_OFFSET_FUTURE,
+	TCA_TDMA_OFFSET_RELATIVE,
+	__TCA_TDMA_MAX,
+};
+
+#define TCA_TDMA_MAX (__TCA_TDMA_MAX - 1)
+
