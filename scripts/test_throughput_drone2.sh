daf632869588e04fab79f42c5e72a8bba9b8a8c8
davidpereira.pereira4 <davidpereira.pereira4@gmail.com>
2024-10-10 14:26:55 +0000

    Correction to test script 2

--- scripts/test_throughput_drone2.sh
+++ scripts/test_throughput_drone2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./add_qdisc.sh test-config-drone2
-iperf3 -c 10.10.10.1 -t 60 -p 5202 > ../docs/logs/iperf-log-latest.txt
+iperf3 -c 10.10.10.1 -t 60 -p 5201 > ../docs/logs/iperf-log-latest.txt
 ./remove_qdisc.sh wlx000f60050e55
