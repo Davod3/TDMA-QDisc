de1d985c8eed4687de9e04a70a3b481d21227a60
davidpereira.pereira4 <davidpereira.pereira4@gmail.com>
2024-10-15 14:57:41 +0000

    File transfer test script corrections

--- scripts/test_file_transfer_drone1.sh
+++ scripts/test_file_transfer_drone1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./add_qdisc.sh test-config-drone1
-dd if=/dev/zero bs=1M count=1000 | ssh pi@10.10.10.2 'dd of=/dev/null'
+dd if=/dev/zero bs=1M count=250 | ssh pi@10.10.10.2 'dd of=/dev/null' 2>&1 | tee ../docs/logs/file-transfer-log.txt
 ./remove_qdisc.sh wlx000f600580e1
