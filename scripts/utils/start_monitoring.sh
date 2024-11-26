dd2739f726e28df503e03282b6b946de1abd76f3
Davod3 <davidpereira.pereira4@gmail.com>
2024-11-26 19:14:22 +0000

    Changes to monitoring script

--- scripts/utils/start_monitoring.sh
+++ scripts/utils/start_monitoring.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#sudo service NetworkManager stop
+sudo service NetworkManager stop
 sudo ifconfig $1 down
 sudo iwconfig $1 mode monitor
 sudo ifconfig $1 up
+sudo iwconfig $1 channel 40
 sudo iwconfig $1 channel 1
 iwconfig
--- scripts/utils/stop_monitoring.sh
+++ scripts/utils/stop_monitoring.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo ifconfig $1 down
-sudo service NetworkManager start
+sudo iwconfig $1 mode managed
+sudo ifconfig $1 up
+sudo service NetworkManager restart
 ifconfig
