4694d5d6c8dd71f1b2886196896cf869e785e3a4
Davod3 <davidpereira.pereira4@gmail.com>
2024-12-03 17:29:55 +0000

    Added test scripts for nodes 5 and 6

---
+++ scripts/no_qdisc_testing/test_throughput_noqdisc_udp_drone6.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd .. # Scripts Folder
+
+iperf3 -c 10.10.10.5 -t 60 -p 5201 -b 0 -u > ../docs/logs/iperf-log-latest.txt
---
+++ scripts/udp_throughput_testing/test_throughput_udp_drone6.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd .. # Scripts FOlder
+
+./utils/add_qdisc.sh test-config-drone6
+iperf3 -c 10.10.10.5 -t 60 -p 5201 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+./utils/remove_qdisc.sh wlan0
