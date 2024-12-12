dfcf67a4e58c93cfd3ee1c1b9542873dd34057de
Davod3 <davidpereira.pereira4@gmail.com>
2024-12-12 18:09:42 +0000

    Added sync test scripts

---
+++ scripts/sync_progression_testing/test_sync_drone4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd .. # Scripts Folder
+
+./utils/add_qdisc.sh test-config-drone4
+iperf3 -c 10.10.10.3 -t 900 -p 5201 -b 0 -u > ../docs/logs/iperf-log-latest.txt &
+
+for ((i=0; i<90; i++))
+do
+    #Sample NTP stats
+    ntpstat | grep 'time correct to' | sed -n 's/.* \([0-9]\+\) ms.*/\1/p' >> ../docs/logs/ntpstat-log.txt
+    sleep 10
+done
+
+./utils/remove_qdisc.sh wlan0
