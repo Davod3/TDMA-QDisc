a3445e0c7bd6ad56e23872832e1925344ce96ec5
davidpereira.pereira4 <davidpereira.pereira4@gmail.com>
2024-12-13 17:38:19 +0000

    Test data drone 5

--- scripts/sync_progression_testing/test_sync_drone5.sh
+++ scripts/sync_progression_testing/test_sync_drone5.sh
@@ -2,6 +2,9 @@
 
 cd .. # Scripts Folder
 
+# Delete old log file
+rm -rfv ../docs/logs/ntpstat-log.txt
+
 ./utils/add_qdisc.sh test-config-drone5
 iperf3 -c 10.10.10.6 -t 900 -p 5201 -b 0 -u > ../docs/logs/iperf-log-latest.txt &
 
