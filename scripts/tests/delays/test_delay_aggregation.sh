94b6c6262e25d2d7d0cda4a49289b7b9ac7e4322
davidpereira.pereira4 <davidpereira.pereira4@gmail.com>
2025-02-21 18:03:09 +0000

    Corrected error with delays test script

--- scripts/tests/delays/test_delay_aggregation.sh
+++ scripts/tests/delays/test_delay_aggregation.sh
@@ -10,6 +10,8 @@ cd udp_throughput_testing
 
 sudo dmesg -C
 
+sudo dmesg -w | grep -E '\[TDMA ROUND\]|\[DELAY\]' > ../../docs/logs/kernel-log-latest.txt &
+
 ./test_throughput_udp_drone$1.sh
 
 cd .. # Scripts Folder
@@ -18,6 +20,6 @@ cd .. # Root Folder
 
 cd docs/logs
 
-sudo dmesg | grep -E '\[TDMA ROUND\]|\[DELAY\]' > ./kernel-log-latest.txt
+sudo pkill -f 'dmesg -w'
 
 ./save_log.sh kernel-log-latest.txt delay-tests 2-nodes drone$1
