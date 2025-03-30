fc7249a516fb76210734888b04243b126b3d104c
Davod3 <davidpereira.pereira4@gmail.com>
2025-03-30 12:49:14 +0100

    Created star topology test script

---
+++ scripts/tests/topologies/test_star_csma.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# $1 - Node ID
+
+cd .. # Tests Folder
+
+cd .. # Scripts Folder
+
+#Array to store iperf3 process ids
+pids=()
+
+parent_node=1
+
+if [ $1 -eq $parent_node ]
+then
+    iperf3 -c 10.10.10.2 -t 60 -p 5201 -b 0 -u &
+    pids+=($!)
+        
+    iperf3 -c 10.10.10.3 -t 60 -p 5201 -b 0 -u &
+    pids+=($!)
+        
+    iperf3 -c 10.10.10.4 -t 60 -p 5201 -b 0 -u &
+    pids+=($!)
+        
+    iperf3 -c 10.10.10.5 -t 60 -p 5201 -b 0 -u &
+    pids+=($!)
+        
+    iperf3 -c 10.10.10.6 -t 60 -p 5201 -b 0 -u &
+    pids+=($!)
+
+else
+    iperf3 -c 10.10.10.1 -t 60 -p 5201 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+fi
+
+# Wait for iperf3 clients to finish transmitting
+for pid in "${pids[@]}"; do
+    wait "$pid"
+done
+
+cd .. # Root Folder
+
+cd docs/logs
+
+./save_log.sh iperf-log-latest.txt star-topology csma drone$1-throughput
---
+++ scripts/tests/topologies/test_star_tdma.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# $1 - Node ID
+
+cd .. # Tests Folder
+
+cd .. # Scripts Folder
+
+#Array to store iperf3 process ids
+pids=()
+
+parent_node=1
+
+sudo dmesg -C
+
+sudo dmesg -w | grep -E '\[TDMA ROUND\]|\[DELAY\]|\[OFFSET\]|\[TOTAL OFFSET\]|\[SLOT_START\]|\[SLOT_END\]|\[PARENT\]|\[SLOT_ID\]|\[DELAY_ON\]|\[DELAY_OFF\]|\[RECEIVED_PACKET\]' > ../docs/logs/kernel-log-latest.txt &
+
+./utils/add_qdisc.sh test-config-drone$1
+
+if [ $1 -eq $parent_node ]
+then
+    iperf3 -c 10.10.10.2 -t 60 -p 5201 -b 0 -u &
+    pids+=($!)
+        
+    iperf3 -c 10.10.10.3 -t 60 -p 5201 -b 0 -u &
+    pids+=($!)
+
+    iperf3 -c 10.10.10.4 -t 60 -p 5201 -b 0 -u &
+    pids+=($!)
+
+    iperf3 -c 10.10.10.5 -t 60 -p 5201 -b 0 -u &
+    pids+=($!)
+
+    iperf3 -c 10.10.10.6 -t 60 -p 5201 -b 0 -u &
+    pids+=($!)
+
+else
+    iperf3 -c 10.10.10.1 -t 60 -p 5201 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+fi
+
+# Wait for iperf3 clients to finish transmitting
+for pid in "${pids[@]}"; do
+    wait "$pid"
+done
+
+./utils/remove_qdisc.sh wlan0
+
+cd .. # Root Folder
+
+cd docs/logs
+
+sudo pkill -f 'dmesg -w'
+
+./save_log.sh kernel-log-latest.txt star-topology tdma drone$1-kernel
+./save_log.sh iperf-log-latest.txt star-topology tdma drone$1-throughput
