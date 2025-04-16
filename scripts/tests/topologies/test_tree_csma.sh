a7afb37fb52aada3eab38893a89aa92e52c3751c
Davod3 <davidpereira.pereira4@gmail.com>
2025-04-16 23:07:27 +0100

    Final preparations for CSMA Tree test

--- scripts/tests/topologies/test_tree_csma.sh
+++ scripts/tests/topologies/test_tree_csma.sh
@@ -9,6 +9,12 @@ cd .. # Scripts Folder
 sudo sysctl -w net.ipv4.conf.all.send_redirects=0
 sudo sysctl -w net.ipv4.conf.wlan0.send_redirects=0
 
+test_duration_s=60
+test_guard_s=$((test_duration_s + 25))
+initial_offset_s=10
+
+echo "Starting..."
+
 if [ $1 -eq '1' ]; then
 
     #Block packets from grandchildren nodes
@@ -24,12 +30,15 @@ if [ $1 -eq '1' ]; then
     #iperf3 -c 10.10.10.1 -t 60 -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
 
     #Check routes with tracepath
-    ping 10.10.10.1
-    ping 10.10.10.2
-    ping 10.10.10.3
-    ping 10.10.10.4
-    ping 10.10.10.5
-    ping 10.10.10.6
+    #ping 10.10.10.1
+    #ping 10.10.10.2
+    #ping 10.10.10.3
+    #ping 10.10.10.4
+    #ping 10.10.10.5
+    #ping 10.10.10.6
+
+    #Sleep goes here
+    sleep $test_guard_s
 
     #Remove new routes
     sudo ip route del 10.10.10.4
@@ -52,13 +61,8 @@ if [ $1 -eq '2' ]; then
     sudo route add -host 10.10.10.3 gw 10.10.10.1 wlan0
     sudo route add -host 10.10.10.6 gw 10.10.10.1 wlan0
 
-    #Check routes with tracepath
-    ping 10.10.10.1
-    ping 10.10.10.2
-    ping 10.10.10.3
-    ping 10.10.10.4
-    ping 10.10.10.5
-    ping 10.10.10.6
+    #Sleep goes here
+    sleep $test_guard_s
 
     sudo ip route del 10.10.10.3
     sudo ip route del 10.10.10.6
@@ -78,13 +82,8 @@ if [ $1 -eq '3' ]; then
     sudo route add -host 10.10.10.4 gw 10.10.10.1 wlan0
     sudo route add -host 10.10.10.5 gw 10.10.10.1 wlan0
 
-    #Check routes with tracepath
-    ping 10.10.10.1
-    ping 10.10.10.2
-    ping 10.10.10.3
-    ping 10.10.10.4
-    ping 10.10.10.5
-    ping 10.10.10.6
+    #Sleep goes here
+    sleep $test_guard_s
 
     sudo ip route del 10.10.10.2
     sudo ip route del 10.10.10.4
@@ -108,13 +107,8 @@ if [ $1 -eq '4' ]; then
     sudo route add -host 10.10.10.3 gw 10.10.10.2 wlan0
     sudo route add -host 10.10.10.6 gw 10.10.10.2 wlan0
 
-    #Check routes with tracepath
-    ping 10.10.10.1
-    ping 10.10.10.2
-    ping 10.10.10.3
-    ping 10.10.10.4
-    ping 10.10.10.5
-    ping 10.10.10.6
+    sleep $initial_offset_s
+    iperf3 -c 10.10.10.1 -t $test_duration_s -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
 
     sudo ip route del 10.10.10.5
     sudo ip route del 10.10.10.1
@@ -140,13 +134,8 @@ if [ $1 -eq '5' ]; then
     sudo route add -host 10.10.10.3 gw 10.10.10.2 wlan0
     sudo route add -host 10.10.10.6 gw 10.10.10.2 wlan0
 
-    #Check routes with tracepath
-    ping 10.10.10.1
-    ping 10.10.10.2
-    ping 10.10.10.3
-    ping 10.10.10.4
-    ping 10.10.10.5
-    ping 10.10.10.6
+    sleep $initial_offset_s
+    iperf3 -c 10.10.10.1 -t $test_duration_s -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
 
     sudo ip route del 10.10.10.1
     sudo ip route del 10.10.10.4
@@ -173,13 +162,8 @@ if [ $1 -eq '6' ]; then
     sudo route add -host 10.10.10.4 gw 10.10.10.3 wlan0
     sudo route add -host 10.10.10.5 gw 10.10.10.3 wlan0
 
-    #Check routes with tracepath
-    ping 10.10.10.1
-    ping 10.10.10.2
-    ping 10.10.10.3
-    ping 10.10.10.4
-    ping 10.10.10.5
-    ping 10.10.10.6
+    sleep $initial_offset_s
+    iperf3 -c 10.10.10.1 -t $test_duration_s -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
 
     sudo ip route del 10.10.10.1
     sudo ip route del 10.10.10.2
@@ -197,4 +181,4 @@ cd .. # Root Folder
 
 cd docs/logs
 
-#./save_log.sh iperf-log-latest.txt star-topology csma drone$1-throughput
+./save_log.sh iperf-log-latest.txt tree-topology csma drone$1-throughput
