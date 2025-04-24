8cb4d7e4045bbfe2e04dde3e2e5729b24279bd5e
Davod3 <davidpereira.pereira4@gmail.com>
2025-04-24 16:17:38 +0100

    Modified start test to force desired network topology

--- scripts/tests/topologies/test_star_csma.sh
+++ scripts/tests/topologies/test_star_csma.sh
@@ -11,8 +11,8 @@ pids=()
 
 parent_node=1
 
-if [ $1 -eq $parent_node ]
-then
+if [ $1 -eq $parent_node ]; then
+    
     iperf3 -c 10.10.10.2 -t 60 -p 5201 -u &
     pids+=($!)
         
@@ -28,8 +28,86 @@ then
     iperf3 -c 10.10.10.6 -t 60 -p 5201 -u &
     pids+=($!)
 
-else
+fi
+
+if [ $1 -eq '2' ]; then
+
+    ./topology_utils/block_node3.sh
+    ./topology_utils/block_node4.sh
+    ./topology_utils/block_node5.sh
+    ./topology_utils/block_node6.sh
+
     iperf3 -c 10.10.10.1 -t 60 -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+
+    ./topology_utils/unblock_node3.sh
+    ./topology_utils/unblock_node4.sh
+    ./topology_utils/unblock_node5.sh
+    ./topology_utils/unblock_node6.sh
+
+fi
+
+if [ $1 -eq '3' ]; then
+
+    ./topology_utils/block_node2.sh
+    ./topology_utils/block_node4.sh
+    ./topology_utils/block_node5.sh
+    ./topology_utils/block_node6.sh
+
+    iperf3 -c 10.10.10.1 -t 60 -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+
+    ./topology_utils/unblock_node2.sh
+    ./topology_utils/unblock_node4.sh
+    ./topology_utils/unblock_node5.sh
+    ./topology_utils/unblock_node6.sh
+
+fi
+
+if [ $1 -eq '4' ]; then
+
+    ./topology_utils/block_node3.sh
+    ./topology_utils/block_node2.sh
+    ./topology_utils/block_node5.sh
+    ./topology_utils/block_node6.sh
+
+    iperf3 -c 10.10.10.1 -t 60 -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+
+    ./topology_utils/unblock_node3.sh
+    ./topology_utils/unblock_node2.sh
+    ./topology_utils/unblock_node5.sh
+    ./topology_utils/unblock_node6.sh
+
+fi
+
+if [ $1 -eq '5' ]; then
+
+    ./topology_utils/block_node3.sh
+    ./topology_utils/block_node4.sh
+    ./topology_utils/block_node2.sh
+    ./topology_utils/block_node6.sh
+
+    iperf3 -c 10.10.10.1 -t 60 -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+
+    ./topology_utils/unblock_node3.sh
+    ./topology_utils/unblock_node4.sh
+    ./topology_utils/unblock_node2.sh
+    ./topology_utils/unblock_node6.sh
+
+fi
+
+if [ $1 -eq '6' ]; then
+
+    ./topology_utils/block_node3.sh
+    ./topology_utils/block_node4.sh
+    ./topology_utils/block_node5.sh
+    ./topology_utils/block_node2.sh
+
+    iperf3 -c 10.10.10.1 -t 60 -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+
+    ./topology_utils/unblock_node3.sh
+    ./topology_utils/unblock_node4.sh
+    ./topology_utils/unblock_node5.sh
+    ./topology_utils/unblock_node2.sh
+
 fi
 
 # Wait for iperf3 clients to finish transmitting
--- scripts/tests/topologies/test_star_tdma.sh
+++ scripts/tests/topologies/test_star_tdma.sh
@@ -15,10 +15,8 @@ sudo dmesg -C
 
 sudo dmesg -w | grep -E '\[TDMA ROUND\]|\[DELAY\]|\[OFFSET\]|\[TOTAL OFFSET\]|\[SLOT_START\]|\[SLOT_END\]|\[PARENT\]|\[SLOT_ID\]|\[DELAY_ON\]|\[DELAY_OFF\]|\[RECEIVED_PACKET\]' > ../docs/logs/kernel-log-latest.txt &
 
-./utils/add_qdisc.sh test-config-drone$1
+if [ $1 -eq $parent_node ]; then
 
-if [ $1 -eq $parent_node ]
-then
     iperf3 -c 10.10.10.2 -t 60 -p 5201 -u &
     pids+=($!)
         
@@ -34,16 +32,112 @@ then
     iperf3 -c 10.10.10.6 -t 60 -p 5201 -u &
     pids+=($!)
 
-else
+fi
+
+if [ $1 -eq '2' ]; then
+
+    ./topology_utils/block_node3.sh
+    ./topology_utils/block_node4.sh
+    ./topology_utils/block_node5.sh
+    ./topology_utils/block_node6.sh
+
+    ./utils/add_qdisc.sh test-config-drone$1
+
     iperf3 -c 10.10.10.1 -t 60 -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+
+    ./utils/remove_qdisc.sh wlan0
+
+    ./topology_utils/unblock_node3.sh
+    ./topology_utils/unblock_node4.sh
+    ./topology_utils/unblock_node5.sh
+    ./topology_utils/unblock_node6.sh
+
+fi
+
+if [ $1 -eq '3' ]; then
+
+    ./topology_utils/block_node2.sh
+    ./topology_utils/block_node4.sh
+    ./topology_utils/block_node5.sh
+    ./topology_utils/block_node6.sh
+
+    ./utils/add_qdisc.sh test-config-drone$1
+
+    iperf3 -c 10.10.10.1 -t 60 -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+
+    ./utils/remove_qdisc.sh wlan0
+
+    ./topology_utils/unblock_node2.sh
+    ./topology_utils/unblock_node4.sh
+    ./topology_utils/unblock_node5.sh
+    ./topology_utils/unblock_node6.sh
+
+fi
+
+if [ $1 -eq '4' ]; then
+
+    ./topology_utils/block_node3.sh
+    ./topology_utils/block_node2.sh
+    ./topology_utils/block_node5.sh
+    ./topology_utils/block_node6.sh
+
+    ./utils/add_qdisc.sh test-config-drone$1
+
+    iperf3 -c 10.10.10.1 -t 60 -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+
+    ./utils/remove_qdisc.sh wlan0
+
+    ./topology_utils/unblock_node3.sh
+    ./topology_utils/unblock_node2.sh
+    ./topology_utils/unblock_node5.sh
+    ./topology_utils/unblock_node6.sh
+
+fi
+
+if [ $1 -eq '5' ]; then
+
+    ./topology_utils/block_node3.sh
+    ./topology_utils/block_node4.sh
+    ./topology_utils/block_node2.sh
+    ./topology_utils/block_node6.sh
+
+    ./utils/add_qdisc.sh test-config-drone$1
+
+    iperf3 -c 10.10.10.1 -t 60 -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+
+    ./utils/remove_qdisc.sh wlan0
+
+    ./topology_utils/unblock_node3.sh
+    ./topology_utils/unblock_node4.sh
+    ./topology_utils/unblock_node2.sh
+    ./topology_utils/unblock_node6.sh
+
+fi
+
+if [ $1 -eq '6' ]; then
+
+    ./topology_utils/block_node3.sh
+    ./topology_utils/block_node4.sh
+    ./topology_utils/block_node5.sh
+    ./topology_utils/block_node2.sh
+
+    ./utils/add_qdisc.sh test-config-drone$1
+
+    iperf3 -c 10.10.10.1 -t 60 -p 520$1 -b 0 -u > ../docs/logs/iperf-log-latest.txt
+
+    ./utils/remove_qdisc.sh wlan0
+
+    ./topology_utils/unblock_node3.sh
+    ./topology_utils/unblock_node4.sh
+    ./topology_utils/unblock_node5.sh
+    ./topology_utils/unblock_node2.sh
+
 fi
 
 # Wait for iperf3 clients to finish transmitting
 for pid in "${pids[@]}"; do
     wait "$pid"
 done
-
-./utils/remove_qdisc.sh wlan0
 
 cd .. # Root Folder
 
