a06032247af2e5a2a24d5a21e7b31c0cf148ec7f
Davod3 <davidpereira.pereira4@gmail.com>
2025-05-02 16:17:27 +0100

    MOre changes

--- scripts/tests/topologies/test_tree_tdma.sh
+++ scripts/tests/topologies/test_tree_tdma.sh
@@ -152,7 +152,7 @@ if [ $1 -eq '4' ]; then
     ./utils/add_qdisc.sh test-config-drone$1
 
     #sleep $initial_offset_s
-    iperf3 -c 10.10.10.2 -t $test_duration_s -p 520$1 -b 1M -u > ../docs/logs/iperf-log-latest.txt
+    iperf3 -c 10.10.10.2 -t $test_duration_s -p 520$1 -b 5M -u > ../docs/logs/iperf-log-latest.txt
 
     ./utils/remove_qdisc.sh wlan0
 
@@ -215,7 +215,7 @@ if [ $1 -eq '6' ]; then
     ./utils/add_qdisc.sh test-config-drone$1
 
     #sleep $initial_offset_s
-    iperf3 -c 10.10.10.3 -t $test_duration_s -p 520$1 -b 1M -u > ../docs/logs/iperf-log-latest.txt
+    iperf3 -c 10.10.10.3 -t $test_duration_s -p 520$1 -b 5M -u > ../docs/logs/iperf-log-latest.txt
 
     ./utils/remove_qdisc.sh wlan0
 
